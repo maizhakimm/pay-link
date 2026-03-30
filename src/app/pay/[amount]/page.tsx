cc9e39ba4f188a8a9acb7170702bde7c7db69c1e
maizhakimm <maizhakim.m@gmail.com>
2026-03-31 03:28:20 +0800

    Update page.tsx

--- src/app/pay/[amount]/page.tsx
+++ src/app/pay/[amount]/page.tsx
@@ -5,16 +5,21 @@ export default function PayPage({ params }: { params: { amount: string } }) {
 
       <h2>Amount: RM {params.amount}</h2>
 
-      <button
-        style={{
-          padding: '12px 24px',
-          backgroundColor: 'black',
-          color: 'white',
-          marginTop: '20px'
-        }}
-      >
-        Pay Now
-      </button>
+     <a
+  href={`https://wa.me/60163352087?text=Hi, I want to pay RM ${params.amount}`}
+  target="_blank"
+>
+  <button
+    style={{
+      padding: '12px 24px',
+      backgroundColor: 'green',
+      color: 'white',
+      marginTop: '20px'
+    }}
+  >
+    Pay via WhatsApp
+  </button>
+</a>
     </main>
   )
 }
