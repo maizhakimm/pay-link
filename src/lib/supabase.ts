e97226bd254f7c71db740cd3777d2ab9bdea8c01
maizhakimm <maizhakim.m@gmail.com>
2026-03-31 12:19:34 +0800

    Update supabase.ts

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,4 +1,4 @@
-import { supabase } from '@/lib/supabase'
+import { createClient } from '@supabase/supabase-js'
 
 const supabaseUrl = 'https://ciaztzqoaxutoftqwiud.supabase.co'
 const supabaseAnonKey = 'sb_publishable_mcsLahm_WOTQRU7PU1aWMQ_INqaQZGC'
