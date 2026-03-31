621ee05a9bf468dd36d7d1890960cd7ce23525ab
maizhakimm <maizhakim.m@gmail.com>
2026-03-31 11:42:33 +0800

    Delete src/lib/supabse.ts

--- src/lib/supabse.ts
+++
@@ -1,6 +0,0 @@
-import { createClient } from '@supabase/supabase-js'
-
-const supabaseUrl = 'https://ciaztzqoaxutoftqwiud.supabase.co'
-const supabaseAnonKey = 'sb_publishable_mcsLahm_WOTQRU7PU1aWMQ_INqaQZGC'
-
-export const supabase = createClient(supabaseUrl, supabaseAnonKey)
