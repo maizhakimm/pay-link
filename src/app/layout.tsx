2a0f0174bde256f17a530fe1046f90a81e03764e
maizhakimm <maizhakim.m@gmail.com>
2026-04-04 02:45:55 +0800

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import "./globals.css";
 
 export const metadata: Metadata = {
-  title: "Pay Link",
+  title: "BayarLink",
   description: "Minimal Next.js App Router starter"
 };
 
