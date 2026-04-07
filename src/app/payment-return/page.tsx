0b8dc90d8bf0a1422a60c8bfcb0b0191bf7e8632
maizhakimm <maizhakim.m@gmail.com>
2026-04-07 15:12:32 +0800

    Update page.tsx

--- src/app/payment-return/page.tsx
+++ src/app/payment-return/page.tsx
@@ -1,24 +1,10 @@
+import { Suspense } from 'react'
 import PaymentReturnClient from './PaymentReturnClient'
 
-type ResolvedSearchParams = {
-  status?: string
-  status_description?: string
-  order_number?: string
-  amount?: string
-  payer_name?: string
-  shop?: string
-  payment_intent_id?: string
-  payment_intent?: string
-}
-
-type PaymentReturnPageProps = {
-  searchParams: Promise<ResolvedSearchParams>
-}
-
-export default async function PaymentReturnPage({
-  searchParams,
-}: PaymentReturnPageProps) {
-  const resolvedSearchParams = await searchParams
-
-  return <PaymentReturnClient searchParams={resolvedSearchParams} />
+export default function PaymentReturnPage() {
+  return (
+    <Suspense fallback={<div style={{ padding: 24 }}>Loading...</div>}>
+      <PaymentReturnClient />
+    </Suspense>
+  )
 }
