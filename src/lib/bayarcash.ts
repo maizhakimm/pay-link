7b3518709402bf7c43c618d33012ae971f961ec9
maizhakimm <maizhakim.m@gmail.com>
2026-04-08 15:00:56 +0800

    Update bayarcash.ts

--- src/lib/bayarcash.ts
+++ src/lib/bayarcash.ts
@@ -1,10 +1,18 @@
 import crypto from 'crypto'
 
+export const BAYARCASH_CHANNELS = {
+  FPX: 1,
+  CARD: 4,
+  SPAYLATER: 7,
+  BOOST_PAYFLEX: 8,
+} as const
+
 function hmacSha256(payload: string, secret: string) {
   return crypto.createHmac('sha256', secret).update(payload).digest('hex')
 }
 
 export function createBayarcashPaymentIntentChecksum(data: {
+  payment_channel: number
   order_number: string
   amount: string
   payer_name: string
@@ -17,10 +25,10 @@ export function createBayarcashPaymentIntentChecksum(data: {
     order_number: data.order_number,
     payer_email: data.payer_email,
     payer_name: data.payer_name,
+    payment_channel: data.payment_channel,
   }
 
   const sortedKeys = Object.keys(payloadData).sort()
-
   const payloadString = sortedKeys
     .map((key) => String(payloadData[key as keyof typeof payloadData]))
     .join('|')
