fca95a1710717c52fe7717633e1aeebac1855411
maizhakimm <maizhakim.m@gmail.com>
2026-04-17 10:43:28 +0800

    Update page.tsx

--- src/app/dashboard/orders/page.tsx
+++ src/app/dashboard/orders/page.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import Layout from '../../../components/Layout'
+import Layout from '../../components/Layout'
 import { useCallback, useEffect, useMemo, useState } from 'react'
 import { supabase } from '../../../lib/supabase'
 
