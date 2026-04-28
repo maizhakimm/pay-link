960bec941df61ee8479fec293da40329e1798758
maizhakimm <maizhakim.m@gmail.com>
2026-04-28 20:07:43 +0800

    Update page.tsx

--- src/app/s/[shopSlug]/page.tsx
+++ src/app/s/[shopSlug]/page.tsx
@@ -15,14 +15,14 @@ type PageProps = {
    ✅ FIX: gunakan ANON KEY
 ========================= */
 function getServerSupabase() {
-  const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL
-  const anonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY
+  const supabaseUrl = process.env.SUPABASE_URL || process.env.NEXT_PUBLIC_SUPABASE_URL
+  const serviceRoleKey = process.env.SUPABASE_SERVICE_ROLE_KEY
 
-  if (!supabaseUrl || !anonKey) {
+  if (!supabaseUrl || !serviceRoleKey) {
     return null
   }
 
-  return createClient(supabaseUrl, anonKey, {
+  return createClient(supabaseUrl, serviceRoleKey, {
     auth: {
       persistSession: false,
       autoRefreshToken: false,
